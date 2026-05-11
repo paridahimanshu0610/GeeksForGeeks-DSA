9b1290a86cfdeef648aadca58f7d0512ad0eeec3
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-11 00:13:34 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.py
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.py
@@ -1,16 +1,16 @@
 class Solution:
-    def dfsTraverse(self, adj, node, currList, visited_nodes):
-        if node in visited_nodes:
-            return currList
-            
-        visited_nodes.add(node)
-        currList.append(node)
+    def traverse(self, node, adj, visited, res):
+        res.append(node)
+        visited.add(node)
         
-        for v in adj[node]:
-            currList = self.dfsTraverse(adj, v, currList, visited_nodes)
+        for e in adj[node]:
+            if e not in visited:
+                self.traverse(e, adj, visited, res)
             
-        return currList
-        
     def dfs(self, adj):
-        visited_nodes = set()
-        return self.dfsTraverse(adj, 0, [], visited_nodes)
+        visited = set()
+        res = []
+        self.traverse(0, adj, visited, res)
+        
+        return res 
+        
