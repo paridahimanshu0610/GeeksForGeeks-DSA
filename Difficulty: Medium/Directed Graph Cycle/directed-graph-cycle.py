45992ca3e20a1c75e7dbcf80086a255a8218f82c
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-05-11 05:28:44 -0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.py
@@ -0,0 +1,29 @@
+class Solution:
+    def dfs(self, adj, node, visited):
+        visited[node] = 1
+        
+        for e in adj[node]:
+            if not visited[e]:
+                if self.dfs(adj, e, visited):
+                    return True
+            elif visited[e] == 1:
+                return True
+                
+        visited[node] = 2
+        
+        return False
+        
+    def isCyclic(self, v, edges):
+        adj = [[] for _ in range(v)]
+        
+        for v1, v2 in edges:
+            adj[v1].append(v2)
+        
+        visited = [0]*v
+        
+        for e in range(v):
+            if not visited[e]:
+                if self.dfs(adj, e, visited):
+                    return True
+                    
+        return False
