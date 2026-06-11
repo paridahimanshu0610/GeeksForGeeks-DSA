41912ab27ecfaa02a6411967719ff89db0cd3697
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-06-11 10:22:41 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/All Longest Common Subsequences/all-longest-common-subsequences.py
+++ Difficulty: Hard/All Longest Common Subsequences/all-longest-common-subsequences.py
@@ -1,47 +1,38 @@
 class Solution:
-    def soFar(self, s1, s2, idx1, idx2, dp):
-        if idx1 < 0 or idx2 < 0:
-            return 0
-
-        if dp[idx1][idx2] is not None:
-            return dp[idx1][idx2]
-
-        if s1[idx1] == s2[idx2]:
-            dp[idx1][idx2] = 1 + self.soFar(s1, s2, idx1-1, idx2-1, dp)
-        else:
-            dp[idx1][idx2] = max(self.soFar(s1, s2, idx1, idx2-1, dp), self.soFar(s1, s2, idx1-1, idx2, dp))
-
-        return dp[idx1][idx2]
-
-    def getAllLCS(self, s1, s2, idx1, idx2, currLCS, all_lcs, maxLen, dp):
-        if len(currLCS) == maxLen:
-            all_lcs.append("".join(currLCS))
+    def getAllSeq(self, s1, s2, idx1, idx2, soFarStr, res, seqLen, dp):
+        if len(soFarStr) == seqLen:
+            res.append("".join(soFarStr))
             return
 
         for char_idx in range(ord('a'), ord('z')+1):
-            done = False
+            found = False
             for i in range(idx1, len(s1)):
-                if chr(char_idx) != s1[i]:
-                    continue
-                for j in range(idx2, len(s2)):
-                    if s1[i] == s2[j] and (dp[i][j] == len(currLCS) + 1 ):
-                        currLCS.append(s1[i])
-                        self.getAllLCS(s1, s2, i+1, j+1, currLCS, all_lcs, maxLen, dp)
-                        currLCS.pop()
-                        done = True
+                if s1[i] == chr(char_idx):
+                    for j in range(idx2, len(s2)):
+                        if (s1[i] == s2[j]) and (dp[i+1][j+1] == len(soFarStr) + 1):
+                            soFarStr.append(s1[i])
+                            self.getAllSeq(s1, s2, i+1, j+1, soFarStr, res, seqLen, dp)
+                            soFarStr.pop()
+                            found = True
+                            break
+
+                    if found:
                         break
-                if done:
-                    break
 
 	def allLCS(self, s1, s2):
-		m, n = len(s1), len(s2)
+	    m, n = len(s1), len(s2)
+	    dp = [[0]*(n+1) for _ in range(m+1)]
 
-		dp = [[None]*n for _ in range(m)]
+	    for idx1 in range(1, m+1):
+	        for idx2 in range(1, n+1):
+	            if s1[idx1-1] == s2[idx2-1]:
+	                dp[idx1][idx2] = 1 + dp[idx1-1][idx2-1]
+	            else:
+	                dp[idx1][idx2] = max(dp[idx1][idx2-1], dp[idx1-1][idx2])
 
-		self.soFar(s1, s2, m-1, n-1, dp)
-		maxLen = dp[m-1][n-1]
-        all_lcs = []
-        
-		self.getAllLCS(s1, s2, 0, 0, [], all_lcs, maxLen, dp)
+	    seqLen = dp[m][n]
+	    res = []
 
-		return all_lcs
+	    self.getAllSeq(s1, s2, 0, 0, [], res, seqLen, dp)
+
+	    return res
