04a1c1072fb04ff3cb526becc74ba48753a159ce
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-01 19:17:40 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.py
@@ -1,41 +1,38 @@
 class Solution:
-    def currBlock(self, a, l, h, dp):
-        # if h - l == 2:
-        #     dp[l][h] = a[h] * a[h-1] * a[l]
-        #     return dp[l][h]
-        # elif h - l < 2:
-        #     dp[l][h] = 0
-        #     return dp[l][h]
-        if h == l:
-            dp[l][h] = 0
+    def leastComp(self, a, l, h, dp):
+        if l == h:
             return 0
         
         if dp[l][h] is not None:
             return dp[l][h]
         
-        mini = float('inf')
+        mini = float('inf')   
         for i in range(l, h):
-            partition = self.currBlock(a, l, i, dp) + self.currBlock(a, i+1, h, dp)
-            curr_prod = a[l-1] * a[i] * a[h]
-            mini = min(mini, curr_prod + partition)
-            
+            combination = self.leastComp(a, l, i, dp) + self.leastComp(a, i+1, h, dp)
+            currProd = (a[l-1]*a[i]*a[h] + combination)
+            mini = min(mini, currProd)
+        
         dp[l][h] = mini
         
-        return dp[l][h]
-        
+        return dp[l][h]    
+    
     def matrixMultiplication(self, a):
         n = len(a)
-        dp = [[0]*n for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
         
         for length in range(2, n):
-            for l in range(1, n - length + 1):
-                h = l + length - 1
-                
+            for l in range(1, n-length+1):
+                h = l+length-1
                 mini = float('inf')
                 for i in range(l, h):
-                    partition = dp[l][i] + dp[i+1][h]
-                    curr_prod = a[l-1] * a[i] * a[h]
-                    mini = min(mini, curr_prod + partition)
+                    if l==i:
+                        dp[l][i] = 0
+                    if i+1 == h:
+                        dp[i+1][h] = 0
+                        
+                    combination = dp[l][i] + dp[i+1][h]
+                    currProd = a[l-1]*a[i]*a[h] + combination
+                    mini = min(mini, currProd)
                     
                 dp[l][h] = mini
         
