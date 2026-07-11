4f4e849370243cbdc817aff904a76aca47d1bb9d
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-11 16:33:56 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.py
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.py
@@ -1,20 +1,22 @@
 from collections import deque
 
 class Solution:
-    def bfs(self, adj):
-        visited = set()
-        dq = deque()
-        dq.appendleft(0)
-        res = [0]
-        visited.add(0)
+    def bfs(self, a):
+        q = deque()
+        visited = [0]*len(a)
+        res = []
         
-        while len(dq) != 0:
-            curr = dq.pop()
+        q.appendleft(0)
+        res.append(0)
+        visited[0] = 1
+        
+        while len(q) != 0:
+            curr = q.pop()
             
-            for e in adj[curr]:
-                if e not in visited:
-                    visited.add(e)
-                    dq.appendleft(e)
+            for e in a[curr]:
+                if not visited[e]:
+                    q.appendleft(e)
                     res.append(e)
+                    visited[e] = 1
         
         return res
