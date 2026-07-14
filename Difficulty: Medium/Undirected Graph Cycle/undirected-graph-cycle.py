a2f134e4430d23d4e5fd46dee752de166f481d84
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-07-14 00:55:06 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
@@ -1,38 +1,29 @@
-from collections import deque
-
 class Solution:
-    def hasCycle(self, adj, src, visited):
-	    dq = deque()
-		
-		dq.appendleft((src, -1))
-		visited[src] = 1
-		
-		
-		while len(dq) != 0:
-		    curr_size = len(dq)
-		    
-		    for _ in range(curr_size):
-		        node, parent = dq.pop()
-		        
-		        for e in adj[node]:
-		            if not visited[e]:
-		                visited[e] = 1
-		                dq.appendleft((e, node))
-		            elif visited[e] and e != parent:
-		                return True
-		 
-		return False 
-		                
-	def isCycle(self, v, edges):
-	    adj = [[] for _ in range(v)]
-	    visited = [0]*v
-
-	    for v1, v2 in edges:
-	        adj[v1].append(v2)
-	        adj[v2].append(v1)
-	
-		for e in range(v):
-		    if not visited[e] and self.hasCycle(adj, e, visited):
-		        return True
-		        
+    def dfsTraverse(self, a, v, visited, parent):
+        visited[v] = 1
+        
+        for nv in a[v]:
+            if not visited[nv]:
+                if self.dfsTraverse(a, nv, visited, v):
+                    return True
+            elif visited[nv] and nv != parent:
+                return True
+                
         return False
+        
+	def isCycle(self, V, edges):
+	    a = [[] for _ in range(V)]
+	    
+	    for u, v in edges:
+	        a[u].append(v)
+	        a[v].append(u)
+	    
+	    visited = [0]*V
+	    
+	    for v in range(V):
+	        parent = None
+	        if not visited[v]:
+	            if self.dfsTraverse(a, v, visited, parent):
+	                return True
+	    
+	    return False
