b83fa99778ee0089d355e7de0132f469bc89434f
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-08-11 20:02:55 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.py
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.py
@@ -1,34 +1,40 @@
-from collections import deque
-
 class Solution:
     def shortestPath(self, V: int, edges: list[list[int]]) -> list[int]:
         adj = [[] for _ in range(V)]
         
         for u,v,wt in edges:
-            adj[u].append((v, wt))
-        
-        stack = []
+            adj[u].append((v,wt))
+            
+        topoSort = []
         visited = [0]*V
         
-        def dfs(curr):
-            visited[curr] = 1
+        def dfs(node):
+            visited[node] = 1
             
-            for nv, _ in adj[curr]:
+            for nv, _ in adj[node]:
                 if not visited[nv]:
                     dfs(nv)
                     
-            stack.append(curr)
+            topoSort.append(node)
             
+        for v in range(V):
+            if not visited[v]:
+                dfs(v)
+        
+        for i in range(V//2):
+            topoSort[i], topoSort[V-i-1] = topoSort[V-i-1], topoSort[i]
+        
+        res = [float('inf')]*V
+        res[0] = 0
+        
+        for node in topoSort:
+            for nv, wt in adj[node]:
+                currDist = res[node] + wt
+                if currDist < res[nv]:
+                    res[nv] = currDist
+        
         for i in range(V):
-            if not visited[i]:
-                dfs(i)
-        
-        dist = [float("inf")]*V
-        dist[0] = 0
-        
-        while len(stack)!=0:
-            curr = stack.pop()
-            for nv, wt in adj[curr]:
-                dist[nv] = min(dist[nv], dist[curr] + wt)
-              
-        return [-1 if e==float("inf") else e for e in dist]
+            if res[i] == float('inf'):
+                res[i] = -1
+                
+        return res
