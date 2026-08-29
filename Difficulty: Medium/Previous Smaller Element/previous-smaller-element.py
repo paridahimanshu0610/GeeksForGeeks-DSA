4139632f623a99dd78060ffa81293a52fa2a27aa
Himanshu Parida <64791582+paridahimanshu0610@users.noreply.github.com>
2026-08-29 13:56:02 -0500

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Previous Smaller Element/previous-smaller-element.py
+++ Difficulty: Medium/Previous Smaller Element/previous-smaller-element.py
@@ -1,20 +1,18 @@
 class Solution:
 	def prevSmaller(self, a):
 	    n = len(a)
-	    res = [None]*n
 	    stack = []
+	    res = [None]*n
 	    
-	    for i in range(n-1, -1, -1):
-	        if len(stack)==0 or a[i] >= a[stack[-1]]:
-	            stack.append(i)
+	    for i in range(n):
+	        while len(stack)>0 and stack[-1]>=a[i]:
+	            stack.pop()
+	            
+	        if len(stack)>0:
+	            res[i] = stack[-1]
 	        else:
-	            while len(stack) != 0 and a[stack[-1]] > a[i]:
-	                res[stack[-1]] = a[i]
-	                stack.pop()
-	            stack.append(i)
-	   
-	    while len(stack) > 0:
-	        res[stack[-1]] = -1
-	        stack.pop()
-	       
+	            res[i] = -1
+	            
+	        stack.append(a[i])
+	        
 	    return res
